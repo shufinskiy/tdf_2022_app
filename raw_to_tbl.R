5ff80c727ce0aecc7ad5e98a28117e9c74b1aa95
Vladislav Shufinskiy <hollywood.90210@mail.ru>
2022-07-25 17:33:41 +0300

    add functoin transform to data.frame

--- raw_to_tbl.R
+++ raw_to_tbl.R
@@ -11,8 +11,9 @@ hms_or_ms <- function(x){
   return(time)
 }
 
-transform_time <- function(file, number_stage){
+transform_time <- function(file){
   f <- readLines(file)
+  stage <- as.integer(stringr::str_extract(file, "(?<=stage)\\d{1,2}+"))
   position <- as.integer(stringr::str_extract(f, "\\d{1,3}"))
   rider <- stringr::str_trim(stringr::str_extract(f, "[A-Z].+(?=\\(\\w+\\))"))
   rider_country <- toupper(stringr::str_extract(f, "(?<=\\()\\w+(?=\\))"))
@@ -28,32 +29,34 @@ transform_time <- function(file, number_stage){
   
   df <- df %>% 
     dplyr::mutate(TIME = dplyr::if_else(TIME == leader_time, TIME, leader_time + TIME),
-                  STAGE = paste0("stage", number_stage))
+                  STAGE = stage)
   return(df)
 }
+
 list_general <- list.files("./raw_data", pattern = "general.txt$", recursive = TRUE, full.names = TRUE)
-test_general <- transform_time(list_general[15], 3)
+general <- dplyr::bind_rows(lapply(list_general, transform_time))
 
 list_team <- list.files("./raw_data", pattern = "team.txt$", recursive = TRUE, full.names = TRUE)
-test_team <- transform_time(list_team[15], 3)
+team <- dplyr::bind_rows(lapply(list_team, transform_time))
 
 list_white <- list.files("./raw_data", pattern = "white.txt", recursive = TRUE, full.names = TRUE)
-test_white <- transform_time(list_white[15], 3)
+white <- dplyr::bind_rows(lapply(list_white, transform_time))
 
-transform_points <- function(file, number_stage){
+transform_points <- function(file){
   f <- readLines(file)
+  stage <- as.integer(stringr::str_extract(file, "(?<=stage)\\d{1,2}+"))
   position <- as.integer(stringr::str_extract(f, "\\d{1,3}"))
   rider <- stringr::str_trim(stringr::str_extract(f, "[A-Z].+(?=\\(\\w+\\))"))
   rider_country <- toupper(stringr::str_extract(f, "(?<=\\()\\w+(?=\\))"))
   points <- as.numeric(stringr::str_extract(f, "\\d+(?=\t\\s*$)"))
   
   df <- dplyr::tibble(POS = position, RIDER = rider, COUNTRY = rider_country, POINTS = points) %>% 
-    dplyr::mutate(STAGE = paste0("stage", number_stage))
+    dplyr::mutate(STAGE = stage)
   return(df)
 }
 
 list_green <- list.files("./raw_data", pattern = "green.txt$", recursive = TRUE, full.names = TRUE)
-test_green <- transform_points(list_green[15], 3)
+green <- dplyr::bind_rows(lapply(list_green, transform_points))
 
 list_peas <- list.files("./raw_data", pattern = "peas.txt$", recursive = TRUE, full.names = TRUE)
-test_peas <- transform_points(list_peas[14], 3)
+peas <- dplyr::bind_rows(lapply(list_peas, transform_points))
